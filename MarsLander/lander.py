aeaaffa06a95c53a18b579bd5ca911183502e30e
Art Mountain <artmountain@btinternet.com>
2021-09-26 15:57:11 +0100

    Better randomization of start paths

--- MarsLander/lander.py
+++ MarsLander/lander.py
@@ -26,22 +26,35 @@ def get_score(x, y, hs, vs, r, landing_min, landing_max, landing_y):
     points = max(0.9 * landing_min + 0.1 * landing_max - x, 0) + max(x - 0.9 * landing_max - 0.1 * landing_min, 0) + max(y - landing_y, 0)
     points += max(abs(hs) - 20, 0) * 25
     points += max(abs(vs) - 40, 0) * 25
-    points += max(abs(r), 0) * 25
+    points += abs(r) * 25
     return round(points)
 
 
-def create_random_population():
+def create_random_population(r_init, p_init):
     population = {}
     for i in range(POPULATION_SIZE):
         chromosome = []
-        current_r = 0
-        current_p = 0
+        r = randint(-90, 90)
+        p = randint(0, 4)
+        r_base = randint(-10, 10)
+        p_base = randint(-1, 1)
+        #r_diff = randint(-15, 15)
+        #p_diff = randint(-1, 1)
+        last_saved_r = r_init
+        last_saved_p = p_init
+        #chromosome = [[r_diff, p_diff]] * CHROMOSOME_LENGTH
+
         for j in range(CHROMOSOME_LENGTH):
-            new_r = min(max(current_r + randint(-15, 15), -90), 90)
-            new_p = min(max(current_p + randint(-1, 1), 0), 4)
-            chromosome.append([new_r - current_r, new_p - current_p])
-            current_r = new_r
-            current_p = new_p
+            chromosome.append([min(15, max(-15, r_base + randint(-15, 15))), min(1, max(-1, p_base + randint(-1, 1)))])
+            """
+            r = r + randint(-15, 15)
+            p = p + randint(-1, 1)
+            new_saved_r = min(15, max(-15, r - last_saved_r))
+            new_saved_p = min(-1, max(1, p - last_saved_p))
+            chromosome.append([min(15, max(-15, new_saved_r - last_saved_r)), min(1, max(-1, new_saved_p - last_saved_p))])
+            last_saved_r = new_saved_r
+            last_saved_p = new_saved_p
+            """
         population[i] = {'chromosome': chromosome, 'calculated': False}
     return population
 
@@ -69,7 +82,7 @@ def evaluate_path(x, y, hs, vs, r, p, land_x, land_y, fuel, chromosome, landing_
         path_y.append(round(y))
         index += 1
         fuel -= p
-    return [state, path_x, path_y, score, fuel]
+    return [state, path_x, path_y, score, fuel, index - 1, r]
 
 
 def plot(population, land_x, land_y, pause_time):
@@ -99,12 +112,12 @@ def solve_lander(x_init, y_init, hs_init, vs_init, r_init, p_init, fuel_init, la
     found_solution = False
     while not found_solution:
         if initial:
-            population = create_random_population()
+            population = create_random_population(r_init, p_init)
             initial = False
         else:
             # Perform generic algorithm. First select the fittest
             sorted_population = sorted(population.items(), key=lambda x: x[1]['score'])
-            number_to_keep = POPULATION_SIZE // 2
+            number_to_keep = POPULATION_SIZE // 4
             removed_ids = [i[0] for i in sorted_population[number_to_keep:]]
 
             # Now breed more organisms
@@ -128,14 +141,16 @@ def solve_lander(x_init, y_init, hs_init, vs_init, r_init, p_init, fuel_init, la
         scores = []
         for organism in population.values():
             if not organism['calculated']:
-                state, path_x, path_y, score, fuel_left = evaluate_path(x_init, y_init, hs_init, vs_init, r_init, p_init, land_x, land_y, fuel_init, organism['chromosome'], landing_area_min, landing_area_max, landing_area_y)
+                state, path_x, path_y, score, fuel_left, last_gene, last_r = evaluate_path(x_init, y_init, hs_init, vs_init, r_init, p_init, land_x, land_y, fuel_init, organism['chromosome'], landing_area_min, landing_area_max, landing_area_y)
                 organism['path_x'] = path_x
                 organism['path_y'] = path_y
                 organism['score'] = score
                 organism['calculated'] = True
+                # Set latest rotation to maximise how upright we are
+                organism['chromosome'][last_gene][0] = max(-15, min(15, organism['chromosome'][last_gene][0] - last_r))
             scores.append(organism['score'])
         scores.sort()
         found_solution = scores[0] == 0
         print(scores)
-        # plot(population, land_x, land_y, 0.5)
+        #plot(population, land_x, land_y, 0.5)
     plot(population, land_x, land_y, 20)
