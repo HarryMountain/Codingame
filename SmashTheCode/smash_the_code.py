ac283bf0c55708728713ffc2f6a806f98c4ebe27
Art Mountain <artmountain@btinternet.com>
2022-03-26 10:36:59 +0000

    Optimize logic, remove temporary arrays and unnecessary len() calculations

--- SmashTheCode/smash_the_code.py
+++ SmashTheCode/smash_the_code.py
@@ -3,13 +3,16 @@ import math
 from copy import deepcopy
 import numpy as np
 
-SCORE_THRESHOLD_MULTIPLIER = 4
+SCORE_THRESHOLD_MULTIPLIER = 2
 FILL_BONUS = 2
 ROTATION_DICT = {0: 1, 1: 0, 2: -1, 3: 0}
 ONE_CELL_MOVES = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+GRID_WIDTH = 6
+GRID_HEIGHT = 12
 
 grid = np.zeros((6, 12), dtype=int)
 visited = np.zeros((6, 12), dtype=int)
+heights = [0, 0, 0, 0, 0, 0]
 # List of actions by [col, rotation]
 possible_things = [
     [0, 0],
@@ -48,19 +51,23 @@ def place_block(block, rotation, grid, col):
         grid[col, 12 - (grid[col, :] == 0).sum()] = block[1]
 
 
-def merge(grid, cp, prev_grid):
+def merge(grid, cp):
     global vis_grid
 
     # Find all blocks
     blocks = []
+    two_blocks = 0
+    three_blocks = 0
     visited.fill(0)
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x, y] > 0 and visited[x, y] == 0 and grid[x, y] != prev_grid[x, y]:
+    for x in range(GRID_WIDTH):
+        for y in range(GRID_HEIGHT):
+            # if grid[x, y] > 0 and visited[x, y] == 0 and grid[x, y] != prev_grid[x, y]:
+            if grid[x, y] > 0 and visited[x, y] == 0:
                 colour = grid[x, y]
                 connected = [[x, y]]
                 changed = True
                 points_to_search = [[x, y]]
+                is_external = False
                 while changed:
                     number_in_block = len(connected)
                     new_points_to_search = []
@@ -69,7 +76,10 @@ def merge(grid, cp, prev_grid):
                             new_point_x = element[0] + move[0]
                             new_point_y = element[1] + move[1]
                             if -1 < new_point_x < 6 and -1 < new_point_y < 12:
-                                if grid[new_point_x, new_point_y] == colour:
+                                this_colour = grid[new_point_x, new_point_y]
+                                if this_colour == 0:
+                                    is_external = True
+                                if this_colour == colour:
                                     new_point = [new_point_x, new_point_y]
                                     if new_point not in connected:
                                         connected.append(new_point)
@@ -77,55 +87,59 @@ def merge(grid, cp, prev_grid):
                                         new_points_to_search.append(new_point)
                     changed = len(connected) > number_in_block
                     points_to_search = new_points_to_search
-                if len(connected) >= 2:
+                if len(connected) >= 4:
                     blocks.append(connected)
+                elif is_external:
+                    if len(connected) == 3:
+                        three_blocks += 1
+                    elif len(connected) == 2:
+                        two_blocks += 1
     # print(blocks, file=sys.stderr, flush=True)
 
     b = 0
     gb = 0
     colours = set()
     for block in blocks:
-        if len(block) >= 4:
-            colours.add(grid[block[0][0], block[0][1]])
-            b += len(block)
-            gb += 8 if len(block) >= 11 else len(block) - 4
-            for point in block:
-                grid[point[0], point[1]] = 0
-                # Delete skull blocks
-                for move in ONE_CELL_MOVES:
-                    new_point = [point[0] + move[0], point[1] + move[1]]
-                    if -1 < new_point[0] < 6 and -1 < new_point[1] < 12:
-                        if grid[new_point[0], new_point[1]] == -1:
-                            grid[point[0], point[1]] = 0
+        colours.add(grid[block[0][0], block[0][1]])
+        b += len(block)
+        gb += 8 if len(block) >= 11 else len(block) - 4
+        for point in block:
+            grid[point[0], point[1]] = 0
+            # Delete skull blocks
+            for move in ONE_CELL_MOVES:
+                new_point = [point[0] + move[0], point[1] + move[1]]
+                if -1 < new_point[0] < 6 and -1 < new_point[1] < 12:
+                    if grid[new_point[0], new_point[1]] == -1:
+                        grid[point[0], point[1]] = 0
 
     # Drop blocks into gaps
-    for x in range(len(grid)):
+    for x in range(GRID_WIDTH):
         y_new_index = 0
-        for y in range(len(grid[x])):
+        for y in range(GRID_HEIGHT):
             if grid[x, y] != 0:
                 colour = grid[x, y]
                 grid[x, y] = 0
                 grid[x, y_new_index] = colour
                 y_new_index += 1
-
+        heights[x] = y_new_index
     # score calculation
     cb = 0 if len(colours) == 1 else 2 ** (len(colours) - 1)
     score = (10 * b) * max(1, min(999, (cp + cb + gb)))
-    block_score = len(blocks) * 5
+    block_score = (two_blocks * 10 + three_blocks * 20) - max(0, max(heights) - 5) * 10
     return score, block_score
 
 
 def turn(grid, block, rotation, col):
-    prev_grid = deepcopy(grid)
+    # prev_grid = deepcopy(grid)
     place_block(block, rotation, grid, col)
     score = 0
     block_score = 0
     cp = 0
     step_score = 1
     while step_score > 0:
-        step_score, block_score = merge(grid, cp, prev_grid)
+        step_score, block_score = merge(grid, cp)
         if step_score > 0:
-            prev_grid = deepcopy(grid)
+            # prev_grid = deepcopy(grid)
             score += step_score
             cp = 8 if cp == 0 else cp * 2
     return score + block_score, cp
@@ -135,7 +149,7 @@ def turn(grid, block, rotation, col):
 while True:
     # print(grid, file=sys.stderr, flush=True)
     blocks = []
-    grid = np.zeros((6, 12), dtype=int)
+    grid.fill(0)
     for i in range(8):
         # color_a: color of the first block
         # color_b: color of the attached block
@@ -175,15 +189,17 @@ while True:
                     score2, cp2 = turn(temp_grid2, blocks[1], rotation2, col2)
                     score2 += score
                     cp2 += cp
-                    score2 += (temp_grid == 0).sum() * FILL_BONUS
+                    score2 += (temp_grid2 == 0).sum() * 1
                     # print(str(col) + ' ' + str(rotation) + ' ' + str(col2) + ' ' + str(rotation2) + ' ' + str(score), file=sys.stderr, flush=True)
+                    # heights = [(x != 0).sum() for x in temp_grid2]
+                    # score2 -= (max(heights) - min(heights)) * 10
                     if cp2 > 0:
                         merge_actions[score2] = [col, rotation]
                     else:
                         other_actions[score2] = [col, rotation]
 
-    # print(merge_actions, file=sys.stderr, flush=True)
-    # print(other_actions, file=sys.stderr, flush=True)
+    print(merge_actions, file=sys.stderr, flush=True)
+    print(other_actions, file=sys.stderr, flush=True)
 
     action = None
     if len(merge_actions) > 0:
