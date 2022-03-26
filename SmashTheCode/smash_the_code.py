fb31954aab045fcf14735cd3d7c3113407c67ca7
Art Mountain <artmountain@btinternet.com>
2022-03-26 09:37:53 +0000

    Silver League

--- SmashTheCode/smash_the_code.py
+++ SmashTheCode/smash_the_code.py
@@ -1,12 +1,12 @@
 import sys
 import math
 from copy import deepcopy
-from random import shuffle
 import numpy as np
 
 SCORE_THRESHOLD_MULTIPLIER = 4
 FILL_BONUS = 2
-rotation_dict = {0: 1, 1: 0, 2: -1, 3: 0}
+ROTATION_DICT = {0: 1, 1: 0, 2: -1, 3: 0}
+ONE_CELL_MOVES = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
 grid = np.zeros((6, 12), dtype=int)
 visited = np.zeros((6, 12), dtype=int)
@@ -38,14 +38,14 @@ possible_things = [
 
 
 def place_block(block, rotation, grid, col):
-    row = 12 - (grid[col,:] == 0).sum()
+    row = 12 - (grid[col, :] == 0).sum()
     if rotation == 3:
         grid[col, row] = block[1]
         grid[col, row + 1] = block[0]
     else:
         grid[col, row] = block[0]
         col += 1 - rotation
-        grid[col, 12 - (grid[col,:] == 0).sum()] = block[1]
+        grid[col, 12 - (grid[col, :] == 0).sum()] = block[1]
 
 
 def merge(grid, cp, prev_grid):
@@ -56,38 +56,31 @@ def merge(grid, cp, prev_grid):
     visited.fill(0)
     for x in range(len(grid)):
         for y in range(len(grid[x])):
-            if grid[x, y] != prev_grid[x, y]:
-                if grid[x, y] > 0 and visited[x, y] == 0:
-                    colour = grid[x, y]
-                    connected = [[x, y]]
-                    changed = True
-                    points_to_search = [[x, y]]
-                    while changed:
-                        number_in_block = len(connected)
-                        new_points_to_search = []
-                        for element in points_to_search:
-                            for move in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
-                                new_point_x = element[0] + move[0]
-                                new_point_y = element[1] + move[1]
-                                new_point = [new_point_x, new_point_y]
-                                if -1 < new_point_x < 6 and -1 < new_point_y < 12 and visited[
-                                    new_point_x, new_point_y] == 0 and new_point not in connected:
-                                    if grid[new_point_x, new_point_y] == colour:
+            if grid[x, y] > 0 and visited[x, y] == 0 and grid[x, y] != prev_grid[x, y]:
+                colour = grid[x, y]
+                connected = [[x, y]]
+                changed = True
+                points_to_search = [[x, y]]
+                while changed:
+                    number_in_block = len(connected)
+                    new_points_to_search = []
+                    for element in points_to_search:
+                        for move in ONE_CELL_MOVES:
+                            new_point_x = element[0] + move[0]
+                            new_point_y = element[1] + move[1]
+                            if -1 < new_point_x < 6 and -1 < new_point_y < 12:
+                                if grid[new_point_x, new_point_y] == colour:
+                                    new_point = [new_point_x, new_point_y]
+                                    if new_point not in connected:
                                         connected.append(new_point)
                                         visited[new_point_x, new_point_y] = 1
                                         new_points_to_search.append(new_point)
-                        changed = len(connected) > number_in_block
-                        points_to_search = new_points_to_search
-                    if len(connected) >= 2:
-                        blocks.append(connected)
-                    # if visited.sum() == number_of_cells:
-                    # return blocks
+                    changed = len(connected) > number_in_block
+                    points_to_search = new_points_to_search
+                if len(connected) >= 2:
+                    blocks.append(connected)
     # print(blocks, file=sys.stderr, flush=True)
-    #return blocks
 
-    #blocks = find_blocks(grid, prev_grid)
-
-    #print(blocks)
     b = 0
     gb = 0
     colours = set()
@@ -99,7 +92,7 @@ def merge(grid, cp, prev_grid):
             for point in block:
                 grid[point[0], point[1]] = 0
                 # Delete skull blocks
-                for move in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+                for move in ONE_CELL_MOVES:
                     new_point = [point[0] + move[0], point[1] + move[1]]
                     if -1 < new_point[0] < 6 and -1 < new_point[1] < 12:
                         if grid[new_point[0], new_point[1]] == -1:
@@ -110,12 +103,13 @@ def merge(grid, cp, prev_grid):
         y_new_index = 0
         for y in range(len(grid[x])):
             if grid[x, y] != 0:
-                grid[x, y_new_index] = grid[x, y]
+                colour = grid[x, y]
                 grid[x, y] = 0
+                grid[x, y_new_index] = colour
                 y_new_index += 1
 
     # score calculation
-    cb = 0 if len(colours) == 1 else 2**(len(colours) - 1)
+    cb = 0 if len(colours) == 1 else 2 ** (len(colours) - 1)
     score = (10 * b) * max(1, min(999, (cp + cb + gb)))
     block_score = len(blocks) * 5
     return score, block_score
@@ -139,7 +133,7 @@ def turn(grid, block, rotation, col):
 
 # game loop
 while True:
-    print(grid, file=sys.stderr, flush=True)
+    # print(grid, file=sys.stderr, flush=True)
     blocks = []
     grid = np.zeros((6, 12), dtype=int)
     for i in range(8):
@@ -168,35 +162,29 @@ while True:
     merge_actions = {}
     other_actions = {}
     for rotation, col in (possible_things if blocks[0][0] != blocks[0][1] else possible_things[:11]):
-        second_col = col + rotation_dict[rotation]
-        for rotation2, col2 in (possible_things if blocks[1][0] != blocks[1][1] else possible_things[:11]):
-            second_col2 = col2 + rotation_dict[rotation2]
-            # Don't go off the top of the grid
-            if grid[col, 9] == 0 and grid[second_col, 9] == 0:
-                temp_grid = deepcopy(grid)
-                score, cp = turn(temp_grid, blocks[0], rotation, col)
-                if temp_grid[col2, 9] == 0 and temp_grid[second_col2, 9] == 0:
-                    score2, cp2 = turn(temp_grid, blocks[1], rotation2, col2)
-                    score += score2
-                    cp += cp2
-                    score += (temp_grid == 0).sum() * FILL_BONUS
-                    # print(str(col) + ' ' + str(rotation) + ' ' + str(col2) + ' ' + str(rotation2) + ' ' + str(score), file=sys.stderr, flush=True)
-                    if cp > 0:
-                        merge_actions[score] = [col, rotation]
-                    else:
-                        other_actions[score] = [col, rotation]
+        second_col = col + ROTATION_DICT[rotation]
+        # Don't go off the top of the grid
+        if grid[col, 10] == 0 and grid[second_col, 10] == 0:
+            temp_grid = deepcopy(grid)
+            score, cp = turn(temp_grid, blocks[0], rotation, col)
 
-    print(merge_actions, file=sys.stderr, flush=True)
-    print(other_actions, file=sys.stderr, flush=True)
-    '''
-    #print(actions, file=sys.stderr, flush=True)
-    scores = sorted(actions.keys())
-    #print(scores, file=sys.stderr, flush=True)
-    if scores[-1] > score_threshold:
-        action = actions[scores[-1]]
-    else:
-        action = actions[scores[0]]
-    '''
+            for rotation2, col2 in (possible_things if blocks[1][0] != blocks[1][1] else possible_things[:11]):
+                second_col2 = col2 + ROTATION_DICT[rotation2]
+                if temp_grid[col2, 10] == 0 and temp_grid[second_col2, 10] == 0:
+                    temp_grid2 = deepcopy(temp_grid)
+                    score2, cp2 = turn(temp_grid2, blocks[1], rotation2, col2)
+                    score2 += score
+                    cp2 += cp
+                    score2 += (temp_grid == 0).sum() * FILL_BONUS
+                    # print(str(col) + ' ' + str(rotation) + ' ' + str(col2) + ' ' + str(rotation2) + ' ' + str(score), file=sys.stderr, flush=True)
+                    if cp2 > 0:
+                        merge_actions[score2] = [col, rotation]
+                    else:
+                        other_actions[score2] = [col, rotation]
+
+    # print(merge_actions, file=sys.stderr, flush=True)
+    # print(other_actions, file=sys.stderr, flush=True)
+
     action = None
     if len(merge_actions) > 0:
         best_merge = max(merge_actions.keys())
