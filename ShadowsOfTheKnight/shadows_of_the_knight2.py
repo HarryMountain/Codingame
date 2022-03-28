9cf26c428dced568eede6828913c29118eec47cf
Art Mountain <artmountain@btinternet.com>
2022-03-28 19:24:57 +0100

    Version before handling jump outside the grid

--- ShadowsOfTheKnight/shadows_of_the_knight2.py
+++ ShadowsOfTheKnight/shadows_of_the_knight2.py
@@ -20,6 +20,14 @@ def distance(x_dif, y_dif):
 
 last_move = None
 while True:
+    # TODO:
+    #  Art : I think the problem is this:
+    # When the range is near the centre it makers sense to try to cut out half the range each time
+    # As the range gets close to the side it's harder to clsoe the range on the edge of the range next to the
+    # edge of the grid
+    # So need to put the next point close to the edge than the middle of the range
+
+
     bomb_dir = input()
     # Process last move
     if last_move == 'horizontal':
@@ -27,62 +35,39 @@ while True:
         if bomb_dir == 'SAME':
             box_x = [int(divide), int(divide)]
         elif (bomb_dir == 'WARMER') == (x > old_x):
-            box_x[0] = math.floor(divide) + 1
+            box_x[0] = max(box_x[0], math.floor(divide) + 1)
         elif (bomb_dir == 'WARMER') == (x < old_x):
-            box_x[1] = math.ceil(divide) - 1
+            box_x[1] = min(box_x[1], math.ceil(divide) - 1)
     elif last_move == 'vertical':
         divide = (old_y + y) / 2
         if bomb_dir == 'SAME':
             box_y = [int(divide), int(divide)]
             y = int(divide)
         elif (bomb_dir == 'WARMER') == (y > old_y):
-            box_y[0] = math.floor(divide) + 1
+            box_y[0] = max(box_y[0], math.floor(divide) + 1)
         elif (bomb_dir == 'WARMER') == (y < old_y):
-            box_y[1] = math.ceil(divide) - 1
+            box_y[1] = min(box_y[1], math.ceil(divide) - 1)
     print(box_x, file=sys.stderr, flush=True)
     print(box_y, file=sys.stderr, flush=True)
     old_x = x
     old_y = y
     if box_x[0] != box_x[1]:
-        if box_x[0] <= x <= box_x[1]:
-            x = sum(box_x) - old_x
-            if x == box_x[1] and x > 0:
-                x -= 1
-            elif x == box_x[0] and x < w - 1:
-                x += 1
-            if x == old_x:
-                x += 1
-        else:
-            x = max(0, min(w - 1, sum(box_x) - old_x))
-            if x == old_x:
-                x += 1
-            # x = int(0.75 * box_x[1] + 0.25 * box_x[0])
-            # x = ((old_x + box_x[0]) // 2) if old_x > box_x[1] else (old_x + box_x[1]) // 2
-            # x = int(box_x[0] * 1 / 3 + box_x[1] * 2 / 3) if old_x < box_x[0] else int(box_x[1] * 1 / 3 + box_x[0] * 2 / 3)
-
+        x = sum(box_x) - old_x
+        if (abs(x - old_x) > 2 and abs(x - old_x) % 2 == 1) or x == old_x:
+            x += -1 if x > 0 else 1
+        x = max(0, min(w - 1, x))
         last_move = 'horizontal'
     else:
-        changed = x != box_x[0]
+        changed = x != box_x[0] # todo : should this be above and x != old_x?
         x = box_x[0]
         if not changed:
-            if box_y[0] != box_y[1]:
-                if box_y[0] <= y <= box_y[1]:
-                    y = sum(box_y) - old_y
-                    if y == box_y[1] and y > 0:
-                        y -= 1
-                    elif y == box_y[0] and y < h - 1:
-                        y += 1
-                    if y == old_y:
-                        y += 1
-                else:
-                    # y = int(0.75 * box_y[1] + 0.25 * box_y[0])
-                    # y = ((old_y + box_y[0]) // 2) if old_y > box_y[1] else (old_y + box_y[1]) // 2
-                    # y = int(box_y[0] * 1 / 3 + box_y[1] * 2 / 3) if old_y < box_y[0] else int(box_y[1] * 1 / 3 + box_y[0] * 2 / 3)
-                    y = max(0, min(h - 1, sum(box_y) - old_y))
-                    if y == old_y:
-                        y += 1
-                last_move = 'vertical'
-            else:
+            if box_y[0] == box_y[1]:
                 y = box_y[0]
-    print('0 1')
-    #print(*[x, y])
+            else:
+                y = sum(box_y) - old_y
+                if (abs(y - old_y) > 2 and abs(y - old_y) % 2 == 1) or y == old_y:
+                    y += -1 if y > 0 else 1
+                y = max(0, min(h - 1, y))
+                last_move = 'vertical'
+
+    print(*[x, y])
