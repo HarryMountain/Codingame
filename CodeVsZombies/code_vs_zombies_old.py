150aeb7e9c6f70638c529970290ae8662dec7aa9
Art Mountain <artmountain@btinternet.com>
2022-07-02 10:57:09 +0100

    Copy old code from before classes introduced

--- CodeVsZombies/code_vs_zombies_old.py
+++ CodeVsZombies/code_vs_zombies_old.py
@@ -12,40 +12,16 @@ for i in range(100):
     FIBBONACCI_SEQUENCE.append(FIBBONACCI_SEQUENCE[-2] + FIBBONACCI_SEQUENCE[-1])
 
 
-def get_distance(thing1, thing2):
+def get_distance(vector):
     # return round(vector.dot(vector))
-    return (thing1.x - thing2.x)**2 + (thing1.y - thing2.y)**2
+    return vector[0]**2 + vector[1]**2
 
 
-def kill_human(humans, target_x, target_y):
-    for human in humans:
-        if human.x == target_x and human.y == target_y:
-            human.is_alive = False
+def remove_array(my_list, item_to_remove):
+    for i in range(len(my_list)):
+        if item_to_remove[0] == my_list[i][0] and item_to_remove[1] == my_list[i][1]:
+            my_list.pop(i)
             break
-
-
-class Zombie:
-    def __init__(self, id, zombie):
-        self.id = id
-        self.is_alive = True
-        self.x = zombie[0]
-        self.y = zombie[1]
-        self.has_target = False
-        self.target_x = None
-        self.target_y = None
-        self.target_ash = False
-        self.move_x = None
-        self.move_y = None
-        self.count_to_ash = 0
-        self.distance_to_ash = -1
-        self.distance_to_target = -1
-
-
-class Human:
-    def __init__(self, human):
-        self.x = human[0]
-        self.y = human[1]
-        self.is_alive = True
 
 
 # Zombie data is [
@@ -59,129 +35,120 @@ class Human:
 #     7 : DistanceToAsh
 class GameState:
     def __init__(self, ash_pos, zombies, humans):
-        self.ash = Human(ash_pos)
+        self.ash_pos = ash_pos
+
         self.zombies = []
         for i in range(len(zombies)):
-            # self.zombies.append([zombies[i], None, False, None, -1, i, 0, get_distance(ash_pos - zombies[i])])
-            self.zombies.append(Zombie(i, zombies[i]))
-            self.zombies[-1].distance_to_ash = get_distance(self.zombies[-1], self.ash)
-        self.humans = []
-        for human in humans:
-            self.humans.append(Human(human))
+            self.zombies.append([zombies[i], None, False, None, -1, i, 0, get_distance(ash_pos - zombies[i])])
+        self.humans = humans
         self.live_humans = len(humans)
         self.live_zombies = len(zombies)
         self.set_zombie_targets()
 
     def set_zombie_targets(self):
         for zombie in self.zombies:
-            if zombie.is_alive:
+            if zombie is not None:
                 # Possibilities are
                 # No target set - choose target from all humans and Ash
                 # On target for a human but need ro recheck whether to target Ash
                 # Targeting Ash - check whether there is a closer human to target
 
-                target_ash = zombie.target_ash
-                target_human = zombie.has_target and not target_ash
+                target_ash = zombie[2]
+                target_human = zombie[1] is not None and not target_ash
                 recalc_move_vector = False
 
-                if not target_ash and zombie.count_to_ash > 0:
+                if not target_ash and zombie[6] > 0:
                     # Don't need to check anything else
-                    zombie.count_to_ash -= 1
+                    zombie[6] -= 1
                 else:
-                    ash_distance = zombie.distance_to_ash
+                    ash_distance = zombie[7]
                     if target_human:
                         # Check if Ash is closer
                         # print('Ash distance : ' + str(ash_distance), file=sys.stderr, flush=True)
-                        if ash_distance < zombie.distance_to_target:
-                            zombie.target_x = self.ash.x
-                            zombie.target_y = self.ash.y
-                            zombie.target_ash = True
-                            zombie.distance_to_target = ash_distance
-                            zombie.has_target = True
+                        if ash_distance < zombie[4]:
+                            zombie[1] = self.ash_pos
+                            zombie[2] = True
+                            zombie[4] = ash_distance
                             recalc_move_vector = True
                         else:
-                            zombie.count_to_ash = ash_distance // 1400
+                            zombie[6] = ash_distance // 1400
                             # print("Hi", ash_distance, zombie[6])
                     else:
                         # Scan through all possible targets including Ash
                         best_human_dist = ash_distance
                         best_human = None
                         for human in self.humans:
-                            distance = get_distance(zombie, human)
+                            distance = get_distance(zombie[0] - human)
                             if distance < best_human_dist:
                                 best_human = human
                                 best_human_dist = distance
                         if best_human is not None:
                             # Switch Zombie to target this human
-                            zombie.target_x = best_human.x
-                            zombie.target_y = best_human.y
-                            zombie.target_ash = False
+                            zombie[1] = best_human
+                            zombie[2] = False
                             # print('Best human distance: ' + str(best_human_dist), file=sys.stderr, flush=True)
-                            zombie.distance_to_target = round(math.sqrt(best_human_dist))
+                            zombie[4] = round(math.sqrt(best_human_dist))
                             # print('Set zombie distance : ' + str(zombie[4]), file=sys.stderr, flush=True)
-                            zombie.has_target = True
                         else:
                             # Switch Zombie to target Ash
-                            zombie.target_x = self.ash.x
-                            zombie.target_y = self.ash.y
-                            zombie.target_ash = True
-                            zombie.distance_to_target = ash_distance
-                            zombie.has_target = True
-                        zombie.count_to_ash = ash_distance // 1400
+                            zombie[1] = self.ash_pos
+                            zombie[2] = True
+                            zombie[4] = ash_distance
+                        zombie[6] = ash_distance // 1400
                         # print("Hello", ash_distance, zombie[6])
                         recalc_move_vector = True
 
                     # Set move vector if needed
                     if recalc_move_vector:
-                        if zombie.distance_to_target > 0:
-                            zombie.move_x = round((zombie.target_x - zombie.x) * 400 / zombie.distance_to_target)
-                            zombie.move_y = round((zombie.target_y - zombie.y) * 400 / zombie.distance_to_target)
+                        if zombie[4] > 0:
+                            zombie[3] = np.array(
+                                ((zombie[1][0] - zombie[0][0]) * 400 / zombie[4],
+                                 (zombie[1][1] - zombie[0][1]) * 400 / zombie[4]),
+                                dtype=int)
                         else:
-                            zombie.move_x = 0
-                            zombie.move_y = 0
+                            zombie[3] = np.array((0, 0), dtype=int)
 
     def move_zombies(self):
         self.set_zombie_targets()
         for zombie in self.zombies:
             if zombie is not None:
-                zombie.x += zombie.move_x
-                zombie.y += zombie.move_y
-                zombie.distance_to_target -= 400
+                zombie[0] = np.add(zombie[0], zombie[3])
+                zombie[4] -= 400
 
     def update(self, move):
         # print(self.ash_pos, file=sys.stderr, flush=True)
         self.move_zombies()
-        self.ash.x = min(16000, max(0, self.ash.x + move[0]))
-        self.ash.y = min(9000, max(0, self.ash.y + move[1]))
-        # print(self.ash_pos)
+        self.ash_pos[0] = min(16000, max(0, self.ash_pos[0] + move[0]))
+        self.ash_pos[1] = min(9000, max(0, self.ash_pos[1] + move[1]))
+        #print(self.ash_pos)
         score = 0
         humans_alive_score = (self.live_humans ** 2) * 10
         # current_num_zombies = len(self.zombies)
         zombies_killed = 0
-        for zombie in self.zombies:
-            if zombie.is_alive:
-                ash_distance = get_distance(self.ash, zombie)
-                zombie.distance_to_ash = ash_distance
+        for i in range(len(self.zombies)):
+            if self.zombies[i] is not None:
+                ash_distance = get_distance(self.ash_pos - self.zombies[i][0])
+                self.zombies[i][7] = ash_distance
                 if ash_distance < 4000000:
-                    zombie.is_alive = False
+                    self.zombies[i] = None
                     zombies_killed += 1
                     self.live_zombies -= 1
-        # self.zombies[:] = (x for x in self.zombies if get_distance(self.ash_pos - x[0]) > 4000000)
-        # zombies_killed = current_num_zombies - len(self.zombies)
+        #self.zombies[:] = (x for x in self.zombies if get_distance(self.ash_pos - x[0]) > 4000000)
+        #zombies_killed = current_num_zombies - len(self.zombies)
         score += humans_alive_score * sum(FIBBONACCI_SEQUENCE[:zombies_killed])
 
-        for zombie in self.zombies:
-            if zombie.is_alive and not zombie.target_ash and zombie.distance_to_target < 400 and zombie.has_target:
+        for zombie in filter(None, self.zombies):
+            if not zombie[2] and zombie[4] < 400 and zombie[1] is not None:
                 # if zombie[1] is None:
-                # print('Zombie killing human ' + str(zombie[1]), file=sys.stderr, flush=True)
-                zombie.x = zombie.target_x
-                zombie.y = zombie.target_y
-                kill_human(self.humans, zombie.target_x, zombie.target_y)
+                    # print('Zombie killing human ' + str(zombie[1]), file=sys.stderr, flush=True)
+                zombie[0] = zombie[1]
+                remove_array(self.humans, zombie[1])
                 self.live_humans -= 1
 
                 # print('Zombie[1] pre ' + str(zombie[0]) + str(zombie[1]), file=sys.stderr, flush=True)
-                zombie.has_target = False
-                zombie.distance_to_target = -1
+                zombie[1] = None
+                zombie[2] = False
+                zombie[4] = -1
                 # print('Zombie[1] after ' + str(zombie[0]) + str(zombie[1]), file=sys.stderr, flush=True)
                 # print('HUMAN DEAD', file=sys.stderr, flush=True)
         return score
@@ -243,8 +210,8 @@ def genetic_algorithm(steps, game_state, max_time_seconds):
     # print('Best score : ' + str(scored_population[0][1]), file=sys.stderr, flush=True)
     generation = 0
     average_generation_time = 0
-    # while max_time_seconds - time.time() + start_time > 2 * average_generation_time: # todo comment out line below for speed test
-    while generation < 20: # todo
+    while max_time_seconds - time.time() + start_time > 2 * average_generation_time: # todo comment out line below for speed test
+    #while generation < 20: # todo
         for j in range(2, POPULATION_SIZE):
             parents = random.sample([l[0] for l in scored_population[:j]], 2)
             child_moves_breed = []
@@ -274,9 +241,9 @@ def genetic_algorithm(steps, game_state, max_time_seconds):
     #print('Steps : ' + str(scored_population[0][0]), file=sys.stderr, flush=True)
     return scored_population[0][0]
 
-# first_go = True
-# last_time = None
-# start_time = None
+first_go = True
+last_time = None
+start_time = None
 initialized = False
 if __name__ == "__main__":
     # game loop
@@ -311,6 +278,7 @@ if __name__ == "__main__":
         #    steps = None
         #if len(state.zombies) > 0:
             #print(state.zombies[0], file=sys.stderr, flush=True)
+        #print("Steps : " + str(steps), file=sys.stderr, flush=True)
         #print('Score : ' + str(score), file=sys.stderr, flush=True)
 
 
