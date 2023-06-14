2bd03c8d2c9dd431c007930bc2cd57a503976bd9
Art Mountain <artmountain@btinternet.com>
2023-06-14 09:25:55 +0100

    Change config constant name

--- CarDriving/test_neural_net.py
+++ CarDriving/test_neural_net.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from CarDriving.codingame_run import get_nn_inputs, convert_inputs_to_actions
-from CarDriving.config import races, MAX_TIMESTEPS
+from CarDriving.config import races, MAX_ROUNDS
 from CarDriving.display_race import plot_pod_paths
 from CarDriving.game import Game
 
@@ -11,7 +11,7 @@ game = Game(checkpoints)
 model = tf.keras.models.load_model('driving_nn_config.h5')
 
 positions = []
-for i in range(MAX_TIMESTEPS):
+for i in range(MAX_ROUNDS):
     next_checkpoint = game.checkpoints[game.next_checkpoint + (1 if game.next_checkpoint < (len(game.checkpoints) - 1) else 0)]
     nn_inputs = get_nn_inputs(game.angle, game.speed, game.position, game.checkpoints[game.next_checkpoint], next_checkpoint)
     nn_outputs = model.predict([nn_inputs])
--- CarDriving/test_neural_net_from_json.py
+++ CarDriving/test_neural_net_from_json.py
@@ -1,5 +1,5 @@
 from CarDriving.codingame_run import get_nn_inputs, convert_inputs_to_actions
-from CarDriving.config import races, MAX_TIMESTEPS
+from CarDriving.config import races, MAX_ROUNDS
 from CarDriving.display_race import plot_race
 from CarDriving.game import Game
 from CarDriving.neural_net import create_nn_from_json
@@ -12,7 +12,7 @@ nn_from_file = create_nn_from_json('saved_nn.json')
 positions = []
 inputs = []
 next_checkpoints = []
-for i in range(MAX_TIMESTEPS):
+for i in range(MAX_ROUNDS):
     next_checkpoint = game.checkpoints[game.next_checkpoint + (1 if game.next_checkpoint < (len(game.checkpoints) - 1) else 0)]
     nn_inputs = get_nn_inputs(game.angle, game.speed, game.position, game.checkpoints[game.next_checkpoint], next_checkpoint)
     nn_outputs = nn_from_file.evaluate(nn_inputs)
